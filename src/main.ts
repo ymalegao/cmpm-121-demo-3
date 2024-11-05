ae7962a25e71d4092914da73d237a55962f5db11
Yash <ymalegao@ucsc.edu>
2024-11-04 21:06:01 -0800

    done with first part but i think im missing something

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 // @deno-types="npm:@types/leaflet@^1.9.14"
-// import leaflet from "leaflet";
+import leaflet from "leaflet";
 
 // Style sheets
 import "leaflet/dist/leaflet.css";
@@ -7,18 +7,88 @@ import "leaflet/dist/leaflet.css";
 // Fix missing marker images
 import "./leafletWorkaround.ts";
 
+import "./style.css";
+
 // // Deterministic random number generator
-// import luck from "./luck.ts";
+import luck from "./luck.ts";
 
-// const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
-const container = document.getElementById("container") || document.body;
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
 
-const button = document.createElement("button");
-button.textContent = "Click Me";
-
-button.addEventListener("click", () => {
-  alert("You clicked the button!");
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
 
-container.appendChild(button);
+leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+playerMarker.bindTooltip("That's you!");
+playerMarker.addTo(map);
+
+let playerPoints = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "No points yet...";
+
+function spawnCache(i: number, j: number) {
+  const origin = OAKES_CLASSROOM;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+  rect.bindPopup(() => {
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+          <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
+          <button id="poke">poke</button>;
+          <button id="give">give</button>`;
+
+    popupDiv.querySelector<HTMLButtonElement>("#poke")!.addEventListener(
+      "click",
+      () => {
+        pointValue--;
+        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+          pointValue.toString();
+        playerPoints++;
+        statusPanel.innerHTML = `${playerPoints} points accumulated`;
+      },
+    );
+
+    popupDiv.querySelector<HTMLButtonElement>("#give")!.addEventListener(
+      "click",
+      () => {
+        playerPoints--;
+        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+          pointValue.toString();
+        pointValue--;
+        statusPanel.innerHTML = `${playerPoints} points accumulated`;
+      },
+    );
+
+    return popupDiv;
+  });
+}
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
